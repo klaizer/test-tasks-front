c73f235f72f8c94f256a2a93b4d8178e1c57c495
klaizer <klaizer@rambler.ru>
2018-12-12 01:54:23 +0200

    #feature1 change project locale to ru-UA;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localeRuUA from '@angular/common/locales/ru-UA';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
-import { HttpClientModule } from '@angular/common/http';
 import { StopsFilterComponent } from './search-page/stops-filter/stops-filter.component';
 import { SharedModule } from './shared/shared.module';
 import { CurrencyFilterComponent } from './search-page/currency-filter/currency-filter.component';
 import { CardListComponent } from './search-page/card-list/card-list.component';
+
+registerLocaleData(localeRuUA);
 
 @NgModule({
   declarations: [
@@ -27,7 +32,9 @@ import { CardListComponent } from './search-page/card-list/card-list.component';
 
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ru-UA' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
